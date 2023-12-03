5f452cc97461af26fd0d694b447a72554c85d2ad
aatayde <alejandro.atayde.jr@gmail.com>
2023-12-03 00:01:06 -0800

    add UI Hierarchy components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
-// import component from "./components";
+import Header from "./components/Header";
+import DebtandFunds from "./components/DebtandFunds";
+import AccountInfo from "./components/AccountInfo";
+import Button from "./components/Button";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <DebtandFunds />
+      <AccountInfo />
+
+      <Button />
+    </div>
+  );
 }
 
 export default App;
