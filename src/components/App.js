769ef1968ba23ed352671e94a3c59203a4a2c39b
aatayde <alejandro.atayde.jr@gmail.com>
2023-12-12 18:54:37 -0800

    add button component to app, import button component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import Accounts from "./Accounts";
+import Button from "./Button";
 
 function App() {
   return (
     <div>
       <Header />
       <Accounts />
+      <Button />
     </div>
   );
 }
