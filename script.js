9b3922447e442b109c1d703c41c1d15d140c3cb3
aatayde <alejandro.atayde.jr@gmail.com>
2024-07-29 19:43:26 -0700

    dynamically update income based on input

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let income = document.getElementById("income");
-let enterIncome = document.getElementById("enterIncome");
 let displayIncome = document.getElementById("displayIncome");
 
-enterIncome.onkeydown = updateDisplayIncome;
-enterIncome.onkeyup = updateDisplayIncome;
-
 function updateDisplayIncome() {
-  let yearlyIncome = this.value * 40 * 52;
+  console.log(income.value);
+  let yearlyIncome = income.value * 40 * 52;
   displayIncome.innerText = `Your estimated yearly income is $${yearlyIncome} before deductions`;
 }
+
+addEventListener("input", updateDisplayIncome);
