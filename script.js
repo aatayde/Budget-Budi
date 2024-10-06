d6c78ec5e4754166571d7269bd7d97fe060270a1
aatayde <alejandro.atayde.jr@gmail.com>
2024-10-06 08:16:40 -0700

    dynamically update list of expenses

--- script.js
+++ script.js
@@ -11,30 +11,14 @@ function updateDisplayIncome() {
   console.log(income.value);
   let yearlyIncome = income.value * 40 * 52;
   displayIncome.innerText = `Your estimated yearly income before deductions is $${yearlyIncome}`;
+  console.log("Monthly Income: $", income.value * 40 * 4);
+  console.log("Bi Weekly Income: $", income.value * 40 * 2);
+  console.log("Weekly Income: $", income.value * 40);
 }
 
 function handleElementExpense() {
   console.log("you clicked the create element expense function");
   createElementExpense();
-}
-
-// Add value to count
-let count = 0;
-
-// Add Expense list
-let expenseListArray = [];
-
-function createElementExpense() {
-  console.log("create new element expense");
-  let newExpense = document.createElement("input");
-  newExpense.type = "number";
-  // newExpense.value = 0;
-  newExpense.id = "expense-" + count;
-  console.log(newExpense.id);
-  expenseList.appendChild(newExpense);
-
-  // Add each expense to a list
-  expenseListArray.push(newExpense);
 }
 
 // Display Sum of Expenses
@@ -49,8 +33,31 @@ function updateDisplayExpenseTotal() {
   displaySumOfExpenses.innerText = sum;
 }
 
+// Add value to count
+let count = 0;
+
+// Add Expense list
+let expenseListArray = [];
+
+function createElementExpense() {
+  console.log("create new element expense");
+  let newExpense = document.createElement("input");
+  newExpense.type = "number";
+  newExpense.placeholder = 0;
+  newExpense.id = "expense-" + count;
+  console.log(newExpense.id);
+  expenseList.appendChild(newExpense);
+
+  // Add each expense to a list
+  expenseListArray.push(newExpense);
+  // WHEN DO I UPDATE THE EXPENSES???
+  newExpense.addEventListener("newExpense", updateDisplayExpenseTotal);
+}
+
 // Dynamically update income
 income.addEventListener("input", updateDisplayIncome);
+// Dynamically update sum of expenses
+newExpense.addEventListener("input", updateDisplayExpenseTotal);
 
 addExpense.addEventListener("click", (e) => {
   console.log(this.className);
@@ -63,6 +70,3 @@ addExpense.addEventListener("click", (e) => {
   // add count as id value of newly created element
   createElementExpense(count);
 });
-
-// dynamically update the sum of expenses
-displaySumOfExpenses.addEventListener("input", updateDisplayExpenseTotal);
